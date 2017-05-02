4351ab7ded43e033d881ba67d00bd88f6d74e126
我不吃植物 <2756145382@qq.com>
2017-05-02 16:33:40 +0800

    Admins

---
+++ Apps/Adminc/Controller/AdminsController.class.php
@@ -0,0 +1,35 @@
+<?php
+namespace Adminc\Controller;
+use Think\Controller;
+
+class AdminsController extends Controller{
+
+    public function index(){
+
+
+    }
+
+    public function add(){
+
+
+    }
+
+    public function edit(){
+
+
+    }
+
+    public function del(){
+
+
+    }
+
+    public function set(){
+
+        
+    }
+
+
+
+
+}
