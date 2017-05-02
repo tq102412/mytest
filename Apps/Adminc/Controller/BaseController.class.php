f218471c71b1f55f45d9840e95223232239bf52d
我不吃植物 <2756145382@qq.com>
2017-05-02 13:40:50 +0800

    2

---
+++ Apps/Adminc/Controller/BaseController.class.php
@@ -0,0 +1,32 @@
+<?php
+namespace Adminc\Controller;
+use Think\Controller;
+
+class BaseController extends Controller{
+
+     public function _initialize(){
+        
+        $user = session('admin_id');
+
+        if(empty($user)){
+            if(IS_AJAX){
+                $this->ajaxReturn(return_array('登陆凭证过期，请重新登陆'),1,0,U('Login/index'));
+            }else{
+                redirect(U('Login/index'));
+            }
+        }
+            
+            
+        $this->set_layout();
+
+    }
+
+
+    private function set_layout(){
+
+        layout('public/_layout');
+
+    }
+
+
+}
--- index.php
+++ index.php
@@ -17,10 +17,10 @@ if(version_compare(PHP_VERSION,'5.3.0','<'))  die('require PHP > 5.3.0 !');
 // 开启调试模式 建议开发阶段开启 部署阶段注释或者设为false
 define('APP_DEBUG',True);
 
-//define('BIND_MODEL','Adminc');11
+define('BIND_MODULE','Adminc');
 
 // 定义应用目录
-define('APP_PATH','./Application/');
+define('APP_PATH','./Apps/');
 
 // 引入ThinkPHP入口文件
 require './ThinkPHP/ThinkPHP.php';
